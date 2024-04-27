530746fb6ac63c83d6f43f6670ae08f5d342125d
Boaz Lavon <boazlavon@gmail.com>
2024-04-27 15:39:00 +0300

    update configs

--- examples/plot_trails_configs.py
+++ examples/plot_trails_configs.py
@@ -75,33 +75,39 @@ def plot_trails(plot_trials_config_path, hyper_params_path):
     regrets_dir_path = os.path.join(results_dir_path, "results_jsons")
 
     bandits = [config for config in plot_trials_config if config["name"] == "bandit"][0]['values']
-    i = 0
+    results = []
     for bandit in bandits:
         bandit_trials_kwargs = get_bandit_trials_kwargs(bandit, plot_trials_config, hyper_params)
         for idx, bandit_kwargs in enumerate(bandit_trials_kwargs):
             result_json_path = get_result_path(bandit, bandit_kwargs, regrets_dir_path)
             if not os.path.exists(result_json_path):
-                print(f"[{i + 1}] Skipping bandit {bandit} with kwargs {bandit_kwargs}")
+                print(f"Skipping bandit {bandit} with kwargs {bandit_kwargs}")
             
-            print(f"[{i + 1}] Plotting bandit {bandit} with kwargs {bandit_kwargs}")
+            result = None
             with open(result_json_path, "r") as f:
-                result = json.load(f)
-
+                result = json.load(f)            
+            
+            if result is not None:
+                results.append(result)
+    
+    i = 0
+    results = sorted(results, key=lambda x: x['regret'][-1])[::-1]
+    for result in results:
             bandit, bandit_kwargs, bandit_regret = result['bandit'], result['kwargs'], result['regret']
             label = get_label(bandit, bandit_kwargs)
             label = f"{bandit}_{i + 1}"
-            plt.plot(range(len(bandit_regret)), bandit_regret, c=col[idx % len(col)], ls='-', label=label)
+            print(f"[{i + 1}] Plotting bandit {bandit} (r={bandit_regret[-1]}) with kwargs {bandit_kwargs} ")
+            plt.plot(range(len(bandit_regret)), bandit_regret, c=col[i % len(col)], ls='-', label=label)
             i += 1
     
     # set plot
     plt.xlabel('time')
     plt.ylabel('regret')
     #plt.legend()
-    plt.legend(loc='upper right', fontsize='small', ncol=2)  
+    plt.legend(loc='lower left', fontsize='small', ncol=2)  
     axes = plt.gca()
     axes.set_ylim([0, 1])
     plt.title("Regret Bound with respect to T")
-    plt.legend()
 
     results_dir_path = os.path.join("results")
     plots_dir_path = os.path.join(results_dir_path, "plots")
